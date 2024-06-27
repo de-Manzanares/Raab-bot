d0394f0059c8b4da59a7630d0845463d41eb8407
de-Manzanares <schauss@dmanz.org>
2024-06-27 14:52:05 -0700

    cleanup

--- include/UCI.h
+++ include/UCI.h
@@ -1,32 +1,78 @@
+/*
+ * Copyright (c) 2024 de-Manzanares
+ * This work is released under the MIT license.
+ */
+
 #ifndef INCLUDE_UCI_H_
 #define INCLUDE_UCI_H_
 
 #include "Board.h"
-#include "Eval.h"
 #include "Node.h"
-#include "Search.h"
 
-#include <iostream>
 #include <string>
 #include <thread>
 
+namespace uciloop {
+
+/**
+ * @brief Does string s contain string has
+ * @param s The container
+ * @param has The value
+ * @return True - the substring is found \n False - the substring is not found
+ */
 bool simon_says(const std::string *s, const std::string &has);
-bool is_maxing(Node *n);
+
+/// @return True - white to move \n False - black to move
+bool is_maxing(const Node *n);
+
+/**
+ * @brief Gives info's to std out
+ * @param update_interval_ms The update interval in milliseconds.
+ */
 void status_update_thread(uint update_interval_ms);
 
+/**
+ * @brief Handles the preamble for the engine in UCI protocol
+ * @param in A pointer to a string containing the input command
+ */
 void preamble(const std::string *in);
-void startpos_moves(Node *n, std::string *in);
 
+/**
+ * @brief Performs moves from UCI
+ * @param n Node holding the board to be mutated.
+ * @param in moves string
+ */
+void startpos_moves(const Node *n, const std::string *in);
+
+/**
+ * @brief Decomposes long algebraic notation
+ * @param string The string representation of the chess move.
+ * @param from The starting square of the move
+ * @param to The target square of the move
+ * @param ch If we are promoting a pown, this is the piece to promote to.
+ */
 void string_to_move(const std::string *string, Square *from, Square *to,
                     char *ch);
+
+/**
+ * @param n The node to convert.
+ * @return long algebraic notation of a node's move.
+ * @note a node's "move" is the move performed that makes this node different
+ * from it's parent node
+ */
 std::string long_algebraic_notation(const Node *n);
+
+} // namespace uciloop
 
 /**
  * @class uci
  * @brief Handles the UCI (Universal Chess Interface) protocol.
  */
 struct uci {
+  // TODO info readouts only work for first go command ...
+  /// @brief The main loop that listens for and processes UCI commands
   static void loop();
+  static uint DEPTH; ///< predefined target depth
 };
 
 #endif // INCLUDE_UCI_H_
