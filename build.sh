b5dc697ee87fe421c5477253a77aa314b3713c81
de-Manzanares <schauss@dmanz.org>
2024-06-27 21:51:17 -0700

    Add Ninja build system support to build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
 BUILD_DIR="build"
+
 if [ -d "$BUILD_DIR" ]; then
   rm -rf "$BUILD_DIR"
 fi
+
 mkdir "$BUILD_DIR"
 echo "Existing build directory replaced"
 cd "$BUILD_DIR"
-cmake ..
-make -j
+
+if command -v ninja >/dev/null 2>&1; then
+	echo "Ninja build system found. Using ninja."
+	BUILD_COMMAND="ninja -j 0 -v"
+	cmake -G Ninja ..
+else
+	BUILD_COMMAND="make -j"
+	cmake ..
+fi
+
+$BUILD_COMMAND
+
 echo "Build completed successfully."
 echo "The executable is in the 'build' directory"
