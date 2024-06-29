4500eec8c3a09f88a51263e60351eca4d726b87c
de-Manzanares <schauss@dmanz.org>
2024-06-29 15:02:08 -0700

    catch2 not required

--- build.sh
+++ build.sh
@@ -3,16 +3,20 @@
 
 BUILD_DIR="build"
 
+# if the 'build' directory exists, remove it
 if [ -d "$BUILD_DIR" ]; then
   rm -rf "$BUILD_DIR"
+  echo "Existing build directory replaced"
 fi
 
+# make directory 'build'
 mkdir "$BUILD_DIR"
-echo "Existing build directory replaced"
-cd "$BUILD_DIR"
+cd "$BUILD_DIR" || exit
 
+# set release flags
 CMAKE_FLAGS="-DCMAKE_CXX_FLAGS=-O3 -DNDEBUG -flto=auto -fuse-linker-plugin -march=native"
 
+# if ninja available, use it, otherwise use make
 if command -v ninja >/dev/null 2>&1; then
 	echo "Ninja build system found. Using ninja."
 	BUILD_COMMAND="ninja -j 0 -v"
@@ -22,7 +26,9 @@ else
 	cmake "$CMAKE_FLAGS" ..
 fi
 
+# build
 $BUILD_COMMAND
 
-echo "Build completed successfully."
-echo "The executable is in the 'build' directory"
+# give infos
+printf "\nBuild completed successfully.\n"
+printf "\nThe executable is in the 'build' directory\n\n"
