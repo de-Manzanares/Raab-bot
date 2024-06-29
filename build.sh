cbaa9650d284329c077d0e4405bae7d0586ce330
de-Manzanares <schauss@dmanz.org>
2024-06-29 12:46:08 -0700

    release flags
    -DCMAKE_CXX_FLAGS="-O3 -DNDEBUG -flto=auto -fuse-linker-plugin -march=native"

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# sorry Windows users ... don't know enough about Windows to help ya
 
 BUILD_DIR="build"
 
@@ -10,13 +11,15 @@ mkdir "$BUILD_DIR"
 echo "Existing build directory replaced"
 cd "$BUILD_DIR"
 
+CMAKE_FLAGS="-DCMAKE_CXX_FLAGS=-O3 -DNDEBUG -flto=auto -fuse-linker-plugin -march=native"
+
 if command -v ninja >/dev/null 2>&1; then
 	echo "Ninja build system found. Using ninja."
 	BUILD_COMMAND="ninja -j 0 -v"
-	cmake -G Ninja ..
+	cmake -G Ninja "$CMAKE_FLAGS" ..
 else
 	BUILD_COMMAND="make -j"
-	cmake ..
+	cmake "$CMAKE_FLAGS" ..
 fi
 
 $BUILD_COMMAND
