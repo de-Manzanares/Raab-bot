7e098a0f643c73bee3a7948756fe6fedcffe1fb9
schauss <schauss@dmanz.org>
2025-09-13 14:59:05 -0700

    clean slate

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,3 @@
-/*
- *     ____              __          __          __
- *    / __ \____ _____ _/ /_        / /_  ____  / /_
- *   / /_/ / __ `/ __ `/ __ \______/ __ \/ __ \/ __/
- *  / _, _/ /_/ / /_/ / /_/ /_____/ /_/ / /_/ / /_
- * /_/ |_|\__,_/\__,_/_.___/     /_.___/\____/\__/
- *
- * Copyright (c) 2024 de-Manzanares
- * This work is released under the MIT license.
- *
- */
+import print_hello;
 
-#include "../include/UCI.h"
-
-int main() {
-  uci::loop();
-  return 0;
-}
+int main() { print_hello(); }
