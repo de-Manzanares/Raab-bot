42b6e867da32dffa78c6c3b87346a3136af60e13
schauss <schauss@dmanz.org>
2025-09-28 17:59:30 -0700

    revert: "feat(search): history heuristic"
    This reverts commit 064c6085f75a9797f86715c44c1b6d864834c4ab.

--- modules/details/move.cpp
+++ modules/details/move.cpp
@@ -48,8 +48,8 @@ std::ostream &operator<<(std::ostream &os, const Move &m)
 
 bool is_repetition(const U64 hash)
 {
-  auto const last = pos_stack.end() - 1;
-  if (const auto first = std::find(pos_stack.begin(), last, hash);
+  auto const last = history.end() - 1;
+  if (const auto first = std::find(history.begin(), last, hash);
       first != last) {
     if (const auto second = std::find(first, last, hash); second != last) {
       return true;
@@ -132,8 +132,8 @@ void move(Board &b, const Move m)
   b.stm = ~b.stm;
   b.t_hash ^= zobrist.stm;
 
-  // update pos_stack
-  pos_stack.push_back(b.t_hash);
+  // update history
+  history.push_back(b.t_hash);
 
   // update phase
   b.phase = set_phase(b);
@@ -191,8 +191,8 @@ void unmove(Board &b, const Move m)
   b.stm = ~b.stm;
   b.t_hash ^= zobrist.stm;
 
-  // update pos_stack
-  pos_stack.pop_back();
+  // update history
+  history.pop_back();
 
   // update phase
   b.phase = set_phase(b);
